af67486264e8a0ccffb59c41719a53407188bfe4
Vinaycherry <137153301+Vinaycherry@users.noreply.github.com>
2023-07-21 09:22:45 +0530

    Add files via upload

---
+++ pet.js
@@ -0,0 +1,12 @@
+let person =
+{
+    name: "SILKY",
+    age:2,
+    gender:"Female",
+    eyecolor:"blue",
+    legs:"4",
+    color:"black",
+    food:["Milk", "Chicken", "Bread"]
+
+}
+console.log(person.food);
